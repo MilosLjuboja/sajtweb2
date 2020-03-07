159f05926f18f4bd0782c390df57dcae7060a584
MilosLjuboja <milos.ljuboja.279.18@ict.edu.rs>
2020-03-08 00:58:28 +0100

    promena

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 window.onload=()=>{
+    //NAVIGACIJA
     
 }
+
+$(document).ready(function(){
+    $('#nav ul li a').click(function(e){
+        e.preventDefault();
+        $(this).next('ul').slideToggle();
+    })
+    $('#cover #naslov').css('opacity',0)
+    $('#cover #naslov').animate({
+        left: '220px',
+        opacity:1
+    },2000)
+})
