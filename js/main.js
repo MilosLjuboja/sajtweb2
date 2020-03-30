f6d6b0630d65d38a83b9c44562fba7f9eeb1310d
MilosLjuboja <milos.ljuboja.279.18@ict.edu.rs>
2020-03-30 02:21:58 +0200

    promena

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 window.onload=()=>{
     //NAVIGACIJA
-    
+    document.getElementById('productLink').addEventListener("click",function(link){
+        link.preventDefault();
+    })
+    //slajder
+    slajder();
 }
-
+var niz=["about1.jpg","baner.jpg","cover.jpg"];
+var index=0;
+function slajder(){
+    var divSlajder=document.getElementById('slajder');
+    divSlajder.style.backgroundImage=`url('../img/${niz[index]}')`
+    if(index<niz.length-1){
+        index++;
+    }
+    else{
+        index=1;
+    }
+    setTimeout(slajder,2000)
+}
 $(document).ready(function(){
     $('#nav ul li a').click(function(e){
-        e.preventDefault();
+        // e.preventDefault();
         $(this).next('ul').slideToggle();
     })
     $('#cover #naslov').css('opacity',0)
